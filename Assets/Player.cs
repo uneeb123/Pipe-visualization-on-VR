d1d924e5bec8adc1c7d762d51916110d4cb1995f
Uneeb Agha <uneeb.i.am@gmail.com>
2016-10-18 15:54:52 -0400

    Adding all files
    Adding all working files

---
+++ Assets/Player.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public PipeSystem pipeSystem;
+    public float velocity;
+
+    private Pipe currentPipe;
+    private float distanceTraveled;
+    private float deltaToRotation;
+    private float systemRotation;
+    private Transform world;
+    private float worldRotation;
+
+    private void Start()
+    {
+        world = pipeSystem.transform.parent;
+        currentPipe = pipeSystem.SetupFirstPipe();
+        deltaToRotation = 360f / (2f * Mathf.PI * currentPipe.CurveRadius);
+        SetupCurrentPipe();
+    }
+
+    private void Update()
+    {
+        float delta = velocity * Time.deltaTime;
+        distanceTraveled += delta;
+        systemRotation += delta * deltaToRotation;
+
+        if (systemRotation >= currentPipe.CurveAngle)
+        {
+            delta = (systemRotation - currentPipe.CurveAngle) / deltaToRotation;
+            currentPipe = pipeSystem.SetupNextPipe();
+            SetupCurrentPipe();
+            deltaToRotation = 360f / (2f * Mathf.PI * currentPipe.CurveRadius);
+            systemRotation = delta * deltaToRotation;
+        }
+
+        pipeSystem.transform.localRotation =
+            Quaternion.Euler(0f, 0f, systemRotation);
+    }
+
+    private void SetupCurrentPipe()
+    {
+        deltaToRotation = 360f / (2f * Mathf.PI * currentPipe.CurveRadius);
+        worldRotation += currentPipe.RelativeRotation;
+        if (worldRotation < 0f)
+        {
+            worldRotation += 360f;
+        }
+        else if (worldRotation >= 360f)
+        {
+            worldRotation -= 360f;
+        }
+        world.localRotation = Quaternion.Euler(worldRotation, 0f, 0f);
+    }
+}
